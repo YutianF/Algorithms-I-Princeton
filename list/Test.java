a9a6590502d76cd37ab746209b0372f346bcedaa
yutian <fengyutian@megvii.com>
2020-03-26 00:28:21 +0800

    Update Test.java

--- list/Test.java
+++ list/Test.java
@@ -1,5 +1,17 @@
 package list;
 
-/* for test purpose */
+import java.util.List;
+import java.util.ListIterator;
+import java.util.ArrayList;
+import java.util.Arrays;
+/* for test purpose */ 
 public class Test{
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7));
+        ListIterator<Integer> listIterator = list.listIterator();
+        listIterator.next();
+        listIterator.add(new Integer(99));
+        System.out.println(list);
+
+    }
 }
