b96defa999ba929b7a20d6bea3cadcc70a883a06
yutian <fengyutian@megvii.com>
2020-04-08 23:44:15 +0800

    .DS_Store banished!

--- w1/hw/Percolation.java
+++ w1/hw/Percolation.java
@@ -3,43 +3,74 @@ package w1.hw;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] isOpen;
+    private WeightedQuickUnionUF uf;
+    private int top, bottom;
+    private int nGrid; 
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n){
+        this.isOpen = new boolean[n][n]; //initiate with default value - false
+        this.uf = new WeightedQuickUnionUF(n*n+2);
+        this.top = 0;
+        this.bottom = n*n+1;
+        this.nGrid = n;
+    }
 
+    //return the index of UF for site (row, col)
+    private int getIndex(int row, int col){
+        return (row-1)*this.nGrid+col;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col){
-
+        int node = getIndex(row, col);
+        if(!isOpen(row, col)){
+            this.isOpen[row-1][col-1] = true;
+            if(row==1) this.uf.union(this.top, node);
+            if(row==this.nGrid) this.uf.union(node,this.bottom);
+            if(row>1 && isOpen(row-1,col)) this.uf.union(node, getIndex(row-1, col));
+            if(col>1 && isOpen(row,col-1)) this.uf.union(node, getIndex(row, col-1));
+            if(row<this.nGrid && isOpen(row+1, col)) this.uf.union(node, getIndex(row+1, col));
+            if(col<this.nGrid && isOpen(row, col+1)) this.uf.union(node, getIndex(row, col+1)); 
+        }
     }
+
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col){
-        return false;
+        return this.isOpen[row-1][col-1];
 
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col){
-        return false;
+        return this.uf.find(this.top) == this.uf.find(getIndex(row, col));
 
     }
 
     // returns the number of open sites
     public int numberOfOpenSites(){
-        return 0;
+        int num=0;
+        for(int row=1; row<=this.nGrid; row++){
+            for(int col=1; col<=this.nGrid; col++){
+                if(isOpen(row, col)) num++;
+            }
+        }
+        return num;
 
     }
 
     // does the system percolate?
     public boolean percolates(){
-        return false;
+        
+        return this.uf.find(this.top) == this.uf.find(this.bottom);
 
     }
 
+
     // test client (optional)
-    public static void main(String[] args){
+    public static void main(final String[] args){
         System.out.println(args[0]);
 
     }
--- w1/hw/PercolationStats.java
+++ w1/hw/PercolationStats.java
@@ -1,39 +1,84 @@
 package w1.hw;
 
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+
 public class PercolationStats {
+    private double[] fractions; //opensite fraction in each experiment
+    private int nTrials;
+    private int nGrid;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials){
+        this.fractions = new double[trials];
+        this.nTrials = trials;
+        this.nGrid = n;
+
+        startTrial();
+    }
+
+    private void startTrial(){
+        outer:
+        for(int i=0; i<this.nTrials; i++){
+            Percolation model = new Percolation(this.nGrid);
+            inner:
+            while(true){
+                int row = StdRandom.uniform(this.nGrid)+1;
+                int col = StdRandom.uniform(this.nGrid)+1;
+                //choose a site uniformly among all blocked sites
+                if(model.isOpen(row, col)) continue inner;
+                else{
+                    
+                    model.open(row, col);
+                    if(model.percolates()) {
+                        fractions[i] = ((double)model.numberOfOpenSites())/(this.nGrid*this.nGrid);
+                        break inner;
+                    }
+
+                }
+
+            }
+            //System.out.printf("%d/%d round - fraction:%f\n",i+1, this.nTrials, fractions[i]);
+
+        }
 
     }
 
     // sample mean of percolation threshold
     public double mean(){
-        return 0;
+        return StdStats.mean(fractions);
 
     }
 
     // sample standard deviation of percolation threshold
     public double stddev(){
-        return 0;
+        return StdStats.stddev(fractions);
 
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo(){
-        return 0;
+         return 
+            mean()-1.96*stddev()/Math.sqrt(this.nTrials);
 
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi(){
-        return 0;
+        return 
+            mean()+1.96*stddev()/Math.sqrt(this.nTrials);
 
     }
 
    // test client (see below)
    public static void main(String[] args){
-
+       int n = Integer.parseInt(args[0]);
+       int trials = Integer.parseInt(args[1]);
+       PercolationStats ps = new PercolationStats(n, trials);
+       StdOut.printf("mean \t= %f\n", ps.mean());
+       StdOut.printf("stddev \t= %f\n", ps.stddev());
+       StdOut.printf("95%% confidence interval \t= [%f, %f]\n", ps.confidenceLo(), ps.confidenceHi());
    }
 
 }
