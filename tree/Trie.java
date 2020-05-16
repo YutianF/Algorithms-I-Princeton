4d40d8c05aee7baf9f2bf29fe44164295d860686
yutian <fengyutian@megvii.com>
2020-05-17 01:49:37 +0800

    add Trie

---
+++ tree/Trie.java
@@ -0,0 +1,61 @@
+package tree;
+
+class Trie {
+    
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+        
+    
+    /**
+     * Inserts a word into the trie. 
+     * Time complexity: O(m); Space Complexity: O(m)
+     * m - word.length()
+     */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+    
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curLetter = word.charAt(i);
+            if (node.containsKey(curLetter)) {
+                node = node.get(curLetter);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
---
+++ tree/TrieNode.java
@@ -0,0 +1,37 @@
+package tree;
+
+class TrieNode {
+    
+    // R links to node children
+    private TrieNode[] links;
+    private final int R = 26;
+    private boolean isEnd;
+    
+    
+    /** Initialize your data structure here. */
+    public TrieNode() {
+        links = new TrieNode[R];
+        
+    }
+    
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+    
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+    
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+    
+    public void setEnd() {
+        isEnd = true;
+    }
+    
+    public boolean isEnd() {
+        return isEnd;
+    }
+
+}
